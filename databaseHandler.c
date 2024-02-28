2a513bf82be57acf7453c7a7c37bd5556c552dfe
BAVDE1 <bavde4@gmail.com>
2024-02-28 17:16:02 +1100

    sqlite3 but for real this time

--- databaseHandler.c
+++ databaseHandler.c
@@ -1,2 +1,18 @@
-#include "sqlite3.h"
+#include <stdio.h>
+#include "sqlite3.c"
 
+#define DB_NAME "internal.db"
+
+
+sqlite3* connectToDB() {
+    sqlite3* db;
+    int open = 0;
+    open = sqlite3_open(DB_NAME, &db);
+    
+    if (open != SQLITE_OK) {
+        printf("ERROR: failed to open db - %s", sqlite3_errmsg(db));
+        return 0;
+    }
+    printf("Successfully connected to database\n");
+    return db;
+}
--- server.c
+++ server.c
@@ -114,6 +114,7 @@ void listenToSocket(int ServerSocket) {
 }
 
 void main() {
+    sqlite3* a = connectToDB();
     // Initialisation
     WSADATA wsaData;
     int startupResult = WSAStartup(MAKEWORD(2, 2), &wsaData);  // winsock ver 2.2
